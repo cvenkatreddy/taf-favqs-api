eef520ed4591e4749feba01269ddeaa93ac9c170
Venkat Reddy Chinthareddy <venkatreddy.chinta@gmail.com>
2024-01-24 12:24:06 +0000

    Update cypress.stg.config.js

--- cypress/configs/cypress.stg.config.js
+++ cypress/configs/cypress.stg.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     baseUrl: "https://favqs.com/api",
     env: {
       apiKey: "05ee79f03d51b71c057573c1e7fcf38f",
+      login: "qaauto.venkatreddyc@gmail.com",
+      password: "d69678dd35f",
     },
   },
 });
